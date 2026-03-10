e9fbcaf4cdc15c630dbfaf075058552ce376863a
anishkumarcs24 <anishkumar.cs24@bmsce.ac.in>
2026-03-10 12:26:57 +0530

    Update priority.c

--- Priority/priority.c
+++ Priority/priority.c
@@ -3,95 +3,129 @@
 
 void priority_np(int n, int at[], int bt[], int pr[])
 {
-    int wt[MAX] = {0}, tat[MAX], rt[MAX], ct[MAX], done[MAX] = {0};
-    int time = 0, count = 0;
+    int wt[MAX]={0}, tat[MAX], rt[MAX], ct[MAX], done[MAX]={0};
+    int order[MAX], start[MAX], end[MAX];
 
-    while (count < n)
+    int time=0, count=0, k=0;
+
+    while(count<n)
     {
-        int idx = -1, min = 9999;
+        int idx=-1, min=9999;
 
-        for (int i = 0; i < n; i++)
-            if (at[i] <= time && !done[i] && pr[i] < min)
+        for(int i=0;i<n;i++)
+            if(at[i]<=time && !done[i] && pr[i]<min)
             {
-                min = pr[i];
-                idx = i;
+                min=pr[i];
+                idx=i;
             }
 
-        if (idx != -1)
+        if(idx!=-1)
         {
-            rt[idx] = time - at[idx];
-            wt[idx] = time - at[idx];
+            start[k]=time;
+            order[k]=idx;
 
-            time += bt[idx];
+            rt[idx]=time-at[idx];
+            wt[idx]=time-at[idx];
 
-            ct[idx] = time;           
-            tat[idx] = ct[idx] - at[idx];
+            time+=bt[idx];
 
-            done[idx] = 1;
+            end[k]=time;
+            ct[idx]=time;
+            tat[idx]=ct[idx]-at[idx];
+
+            done[idx]=1;
             count++;
+            k++;
         }
         else
             time++;
     }
 
-    printf("\nPID\tAT\tBT\tPR\tCT\tWT\tTAT\tRT\n");
-    for (int i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-               i + 1, at[i], bt[i], pr[i], ct[i], wt[i], tat[i], rt[i]);
-}
+    printf("\nGantt Chart:\n|");
+    for(int i=0;i<k;i++)
+        printf(" P%d |",order[i]+1);
 
+    printf("\n%d",start[0]);
+    for(int i=0;i<k;i++)
+        printf("   %d",end[i]);
+
+    printf("\n\nPID\tAT\tBT\tPR\tCT\tWT\tTAT\tRT\n");
+
+    for(int i=0;i<n;i++)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        i+1,at[i],bt[i],pr[i],ct[i],wt[i],tat[i],rt[i]);
+}
 
 
 void priority_p(int n, int at[], int bt[], int pr[])
 {
-    int rt_bt[MAX], wt[MAX] = {0}, tat[MAX], rt[MAX], ct[MAX];
+    int rt_bt[MAX], wt[MAX]={0}, tat[MAX], rt[MAX], ct[MAX];
+    int gantt[200];
+    int g=0;
 
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        rt_bt[i] = bt[i];
-        rt[i] = -1;
+        rt_bt[i]=bt[i];
+        rt[i]=-1;
     }
 
-    int time = 0, complete = 0;
+    int time=0, complete=0;
 
-    while (complete < n)
+    while(complete<n)
     {
-        int idx = -1, min = 9999;
+        int idx=-1, min=9999;
 
-        for (int i = 0; i < n; i++)
-            if (at[i] <= time && rt_bt[i] > 0 && pr[i] < min)
+        for(int i=0;i<n;i++)
+            if(at[i]<=time && rt_bt[i]>0 && pr[i]<min)
             {
-                min = pr[i];
-                idx = i;
+                min=pr[i];
+                idx=i;
             }
 
-        if (idx != -1)
+        if(idx!=-1)
         {
-            if (rt[idx] == -1)
-                rt[idx] = time - at[idx];
+            gantt[g++]=idx;
+
+            if(rt[idx]==-1)
+                rt[idx]=time-at[idx];
 
             rt_bt[idx]--;
             time++;
 
-            if (rt_bt[idx] == 0)
+            if(rt_bt[idx]==0)
             {
                 complete++;
-
-                ct[idx] = time;            // Completion Time
-                tat[idx] = ct[idx] - at[idx];
-                wt[idx] = tat[idx] - bt[idx];
+                ct[idx]=time;
+                tat[idx]=ct[idx]-at[idx];
+                wt[idx]=tat[idx]-bt[idx];
             }
         }
         else
+        {
+            gantt[g++]=-1;
             time++;
+        }
     }
 
-    printf("\nPID\tAT\tBT\tPR\tCT\tWT\tTAT\tRT\n");
-    for (int i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-               i + 1, at[i], bt[i], pr[i], ct[i], wt[i], tat[i], rt[i]);
-}
+    printf("\nGantt Chart:\n|");
+    for(int i=0;i<g;i++)
+    {
+        if(gantt[i]==-1)
+            printf(" idle |");
+        else
+            printf(" P%d |",gantt[i]+1);
+    }
 
+    printf("\n0");
+    for(int i=1;i<=g;i++)
+        printf(" %d",i);
+
+    printf("\n\nPID\tAT\tBT\tPR\tCT\tWT\tTAT\tRT\n");
+
+    for(int i=0;i<n;i++)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        i+1,at[i],bt[i],pr[i],ct[i],wt[i],tat[i],rt[i]);
+}
 
 
 int main()
@@ -100,28 +134,23 @@ int main()
     int at[MAX], bt[MAX], pr[MAX];
 
     printf("Processes: ");
-    scanf("%d", &n);
+    scanf("%d",&n);
 
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        printf("AT BT PR for P%d: ", i + 1);
-        scanf("%d%d%d", &at[i], &bt[i], &pr[i]);
+        printf("AT BT PR for P%d: ",i+1);
+        scanf("%d%d%d",&at[i],&bt[i],&pr[i]);
     }
 
     printf("\n1. Priority Non-Preemptive");
     printf("\n2. Priority Preemptive");
     printf("\nChoice: ");
-    scanf("%d", &choice);
+    scanf("%d",&choice);
 
-    switch (choice)
+    switch(choice)
     {
-    case 1:
-        priority_np(n, at, bt, pr);
-        break;
-
-    case 2:
-        priority_p(n, at, bt, pr);
-        break;
+        case 1: priority_np(n,at,bt,pr); break;
+        case 2: priority_p(n,at,bt,pr); break;
     }
 
     return 0;
